19ee0ee8908c2e27c624105ea71271b460863db8
marijnvk <vk.marijn@gmail.com>
2016-09-19 20:54:44 +0200

    Let's also not forget to include all files next commit.

---
+++ src/main/java/group2/webinfbot/WebInfBot.java
@@ -0,0 +1,52 @@
+package group2.webinfbot;
+
+import twitter4j.StallWarning;
+import twitter4j.Status;
+import twitter4j.StatusDeletionNotice;
+import twitter4j.StatusListener;
+import twitter4j.TwitterStream;
+import twitter4j.TwitterStreamFactory;
+
+/**
+ *
+ * @author m.s.v.knippenberg
+ */
+public class WebInfBot {
+
+    public static void main(String[] args) {
+        StatusListener listener;
+        listener = new StatusListener() {
+            @Override
+            public void onStatus(Status status) {
+                System.out.println(status.getUser().getName() + " : " + status.getText());
+            }
+
+            @Override
+            public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {
+            }
+
+            @Override
+            public void onTrackLimitationNotice(int numberOfLimitedStatuses) {
+            }
+
+            @Override
+            public void onException(Exception ex) {
+                ex.printStackTrace();
+            }
+
+            @Override
+            public void onScrubGeo(long l, long l1) {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+
+            @Override
+            public void onStallWarning(StallWarning sw) {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+        };
+        TwitterStream twitterStream = new TwitterStreamFactory().getInstance();
+        twitterStream.addListener(listener);
+        // sample() method internally creates a thread which manipulates TwitterStream and calls these adequate listener methods continuously.
+        twitterStream.sample();
+    }
+}
